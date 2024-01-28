3c20b8c19dcf9fbcd9e44f6bb21ecb20e1648683
Oliver Hewison <hewisonoliver@gmail.com>
2024-01-28 14:40:12 +0000

    Completed clone hero mechanics

--- Assets/Scripts/GuitarHero/GuitarHeroNoteObject.cs
+++ Assets/Scripts/GuitarHero/GuitarHeroNoteObject.cs
@@ -1,23 +1,44 @@
+using DG.Tweening;
 using UnityEngine;
 
 namespace GuitarHero
 {
     public class GuitarHeroNoteObject : MonoBehaviour
     {
+        public Renderer renderer;
         public GuitarHeroNote.NoteType noteType = GuitarHeroNote.NoteType.Strum;
         public float time;
         public bool active = true;
 
+        private static readonly int shader_EmissiveIntensity = Shader.PropertyToID("_Emissive_Intensity");
+        private static readonly int shader_Alpha = Shader.PropertyToID("_Alpha");
+
         public void Hit()
         {
             active = false;
-            gameObject.SetActive(false);
+            var m = renderer.material;
+            m.DOFloat(10.0F, shader_EmissiveIntensity, 0.05F);
+            m.DOFloat(0.0F, shader_Alpha, 0.1F).OnComplete(() =>
+            {
+                gameObject.SetActive(false);
+                Destroy(m);
+            });
         }
 
         public void Missed()
         {
             active = false;
-            gameObject.SetActive(false);
+            var m = renderer.material;
+            m.DOFloat(0.0F, shader_Alpha, 0.1F).OnComplete(() =>
+            {
+                gameObject.SetActive(false);
+                Destroy(m);
+            });
+        }
+
+        private void OnValidate()
+        {
+            renderer = GetComponent<Renderer>();
         }
     }
 }
--- Assets/Scripts/GuitarHero/GuitarTrackObject.cs
+++ Assets/Scripts/GuitarHero/GuitarTrackObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,11 +6,17 @@ namespace GuitarHero
 {
     public class GuitarTrackObject : MonoBehaviour
     {
-        public GuitarHeroNoteObject notePrefab;
+        public GuitarHeroNoteObject notePrefab, tapNotePrefab, slideNotePrefab;
         public List<GuitarHeroNoteObject> notes = new();
         public void AddNote(double time, GuitarHeroNote.NoteType noteType, float fretboardTime, float fretboardLength)
         {
-            var note = Instantiate(notePrefab, transform);
+            var note = Instantiate(noteType switch
+            {
+                GuitarHeroNote.NoteType.Strum => notePrefab,
+                GuitarHeroNote.NoteType.Tap => tapNotePrefab,
+                GuitarHeroNote.NoteType.Slide => slideNotePrefab,
+                _ => throw new ArgumentOutOfRangeException(nameof(noteType), noteType, null)
+            }, transform);
             note.transform.localPosition =
                 new Vector3(0.0F, 0.0F, (((float)time / fretboardTime) * fretboardLength));
             note.noteType = noteType;
